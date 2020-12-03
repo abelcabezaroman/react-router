05fb7034a399edb6d9988744a139da9b474e6d90
acabezar <uzumaki1995>
2020-12-03 16:27:38 +0100

    changes

--- src/pages/UsersPage/pages/UsersDetailPage/UsersDetailPage.jsx
+++ src/pages/UsersPage/pages/UsersDetailPage/UsersDetailPage.jsx
@@ -1,13 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import { useLocation, useParams, useRouteMatch, useHistory } from 'react-router-dom';
 import { users } from '../../../../shared/consts/users';
 
 export default function UsersDetailPage() {
 
     const { idUser } = useParams();
 
+    const history = useHistory();
+    console.log('useRouteMatch', useRouteMatch())
+    console.log('useLocation', useLocation())
+    console.log('useHistory', useHistory())
+
+
     return (
         <div>
+            {/* <button onClick={useHistory().goBack}>Go back</button> //MAL */}
+            <button onClick={() => {history.push('/users')}}>Go back</button>
             {users.map((user, index) => {
                 if (user.id === Number(idUser)) {
                     return <div key={index}>
